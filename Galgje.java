8a5c7838f59cf49357e2dbcab2f79a52f4414984
felixvl <digitaleservice@gmail.com>
2018-01-09 16:13:01 +0100

    af

--- Galgje.java
+++ Galgje.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Galgje {
 	public static String teRadenWoord = "NogNietsIngevuld";
+	public static StringBuilder voortgang = new StringBuilder("****");
 	public static void main(String args[]) {
 		System.out.println("Welkom Bij Galgje");
 		System.out.println("Het te raden woord: ");
@@ -17,12 +18,16 @@ public class Galgje {
 	    System.out.println("Het spel is klaar");
 	}
 	public static void checkenOfLetterBestaat(String i2) {
+		int positie = teRadenWoord.indexOf(i2);
 		System.out.println("Uw ingevoerde letter is:"+ i2);
 		if(teRadenWoord.contains(i2)) {
 			System.out.println("ja, komt voor");
+			System.out.println(" op positie"+teRadenWoord.indexOf(i2));
+			voortgang.replace(positie, positie+1, i2);
 		}else {
 			System.out.println("nee, komt niet voor");	    	
 		}
+		System.out.println(voortgang);
 	}
 }
 
