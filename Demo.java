73239729fd951b79401e1a8f22ad916e09670302
felixvl <digitaleservice@gmail.com>
2018-01-16 22:23:59 +0100

    weer wat

--- Demo.java
+++ Demo.java
@@ -12,24 +12,39 @@ class Demo{
 class MonoPoly{
 	ArrayList<Stad> steden = new ArrayList();
 	void starten() {
-		System.out.println(dobbelsteenGooien());
 		toonBordspel();
+		toonStraat();
 	}
-	int dobbelsteenGooien() {
+	void toonStraat() {
+		int getal = dobbelsteenGooien(steden.size());
+		System.out.println("STAD GEWORPEN");
+		System.out.println(steden.get(getal).naam);
+		int getalstraat = dobbelsteenGooien(steden.get(getal).straten.size());
+		Straat deStraat = steden.get(getal).straten.get(getalstraat);
+		deStraat.gekocht = true;
+		toonBordspel();
+		
+	}
+	int dobbelsteenGooien(int aantal) {
 		Random random = new Random();
-		return (random.nextInt(6)+1);
+		return (random.nextInt(aantal));
 	}
 	void init() {
-		steden.add(new Stad("Amsterdam"));
-		steden.add(new Stad("Utrecht"));
-		steden.add(new Stad("Ons Dorp"));
-		steden.add(new Stad("Leiden"));
-		steden.add(new Stad("Groningen"));
-		steden.add(new Stad("Zwolle"));
+		Stad stadamsterdam = new Stad("Amsterdam");
+		stadamsterdam.straten.add(new Straat("Kalverstraat", 100));
+		stadamsterdam.straten.add(new Straat("Leidsche Plein", 90));
+		steden.add(stadamsterdam);
+		Stad stadUtrecht = new Stad("Utrecht");
+		stadUtrecht.straten.add(new Straat("Neude", 80));
+		stadUtrecht.straten.add(new Straat("Vreedenburg", 0));
+		steden.add(stadUtrecht);
 	}
 	void toonBordspel() {
 		for(Stad stad : steden) {
 			System.out.println(stad.naam);
+			for(Straat straat : stad.straten) {
+				straat.oplezen();
+			}
 		}
 	}
 }
@@ -39,8 +54,19 @@ class Speler{
 }
 class Stad{
 	String naam;
+	ArrayList<Straat> straten = new ArrayList();
 	Stad(String naam){this.naam = naam;}
 }
 class Straat{
-	
+	boolean gekocht = false;
+	String straatnaam;
+	int prijs;
+	Straat(String straatnaam, int prijs){
+		this.straatnaam = straatnaam;
+		this.prijs = prijs;
+	}
+	void oplezen() {
+		if(gekocht) {System.out.print("GEKOCHT: ");}
+		System.out.println("Naam Straat: "+ straatnaam + " prijs "+ prijs);
+	}
 }
